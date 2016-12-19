15c8a10dbd757739388e7f00589bbd332a52a97a
wulfihm <hans-martin.wulfmeyer@st.ovgu.de>
2016-12-19 23:16:19 +0100

    header

--- include/body.h
+++ include/body.h
@@ -10,11 +10,26 @@
 #include "glm/glm.hpp"
 using namespace glm;
 
+static const float G = 6.67408e-11;	// gravitational constant
+static float EPS = 3e4;
+
 class Body {
+
 public:
+
+	Body(float m, vec3 p, vec3 v);
+	~Body();
+
+	void addForceVelocity(Body p);
+	void updatePosition(float dt);
+
+	float mass;
 	vec3 position;
 	vec3 velocity;
+
 private:
+	// default constructor
+	Body();
 
 };
 
